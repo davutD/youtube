c921ec2fbfe59447b35d3602a5c08c5e40ac6f12
davutD <durmaz.dvt@gmail.com>
2025-02-15 00:46:55 +0300

    adding node modules

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 class User {
-  constructor(name, surname) {
+  constructor(name, surname, email) {
     this.name = name
     this.surname = surname
+    this.email = email
   }
 }
+
+class Video {
+  constructor(title, description, videoUrl) {
+    this.title = title
+    this.description = description
+    this.videoUrl = videoUrl
+  }
+}
+
+const davut = new User('Davut', 'Durmaz', 'durmaz.dvt@gmail.com')
+console.log(JSON.stringify(davut, null, 2))
