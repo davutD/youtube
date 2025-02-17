750abc67938b5b93497c64c6159f8774497246a9
davutD <durmaz.dvt@gmail.com>
2025-02-17 04:57:56 +0300

    adding subscription to the models

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Video = require('./models/video')
 const flatted = require('flatted')
 
 const davut = new User('Davut', 'Durmaz', 'durmaz.dvt@gmail.com')
+const kaan = new User('Kaan', 'Yurdakos', 'yurdakos.kaan@gmail.com')
 
 davut.createVideo(
   'JavaScript OOP',
@@ -12,11 +13,19 @@ davut.createVideo(
   ['JavaScript', 'OOP']
 )
 
-davut.createVideo(
+kaan.createVideo(
   'JavaScript Array Methods',
   'JavaScript Array Methods',
   'https://www.youtube.com/watch?v=OvD7J3Hn9Uw',
   ['JavaScript', 'Array', 'Methods']
 )
 
-console.log(flatted.parse(flatted.stringify(davut.videos[0])))
+// console.log(flatted.parse(flatted.stringify(davut.videos)))
+// console.log(flatted.parse(flatted.stringify(kaan.videos)))
+
+davut.subscribeUser(kaan)
+davut.makeComment(kaan.videos[0], 'Great video!')
+davut.dislikeVideo(kaan.videos[0])
+console.log(flatted.parse(flatted.stringify(kaan.videos[0])))
+davut.likeVideo(kaan.videos[0])
+console.log(flatted.parse(flatted.stringify(kaan.videos[0])))
--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ class User {
     this.surname = surname
     this.email = email
     this.videos = []
+    this.subscribers = []
   }
 
   createVideo(title, description, videoUrl, tags) {
@@ -18,6 +19,8 @@ class User {
 
     video.likedUsers.push(this.email)
     video.dislikedUsers.splice(video.dislikedUsers.indexOf(this.email), 1)
+    video.countDislikes <= 0 ? video.countDislikes == 0 : video.countDislikes--
+    video.countLikes++
   }
   dislikeVideo(video) {
     if (video.dislikedUsers.includes(this.email))
@@ -25,9 +28,24 @@ class User {
 
     video.likedUsers.splice(video.likedUsers.indexOf(this.email), 1)
     video.dislikedUsers.push(this.email)
+    video.countDislikes++
+    video.countLikes <= 0 ? video.countLikes == 0 : video.countLikes--
   }
   makeComment(video, comment) {
     video.comments.push({ email: this.email, comment: comment })
+  }
+
+  subscribeUser(user) {
+    if (this.subscribers.includes(user))
+      throw new Error(`${user.email} already subscribed.`)
+
+    this.subscribers.push(user)
+  }
+  unsubscribeUser(user) {
+    if (!this.subscribers.includes(user))
+      throw new Error(`${user.email} is not subscribed.`)
+
+    this.subscribers.splice(this.subscribers.indexOf(user), 1)
   }
 }
 
