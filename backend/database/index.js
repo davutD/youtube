63f9eb6a884d56f04226261c84d304a864382ecb
davutD <durmaz.dvt@gmail.com>
2025-06-20 00:50:36 +0300

    Export commentDatabase from index.js

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,7 +1,9 @@
 const userDatabase = require('./user-database')
 const videoDatabase = require('./video-database')
+const commentDatabase = require('./comment-database')
 
 module.exports = {
   userDatabase,
   videoDatabase,
+  commentDatabase,
 }
