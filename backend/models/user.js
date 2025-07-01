5eb420f6383dfaeaf56bcc58dba512ebcea8659f
davutD <durmaz.dvt@gmail.com>
2025-07-02 01:12:02 +0300

    Add a password hashing middleware and comparison method to user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const autopopulate = require('mongoose-autopopulate')
+const bcrypt = require('bcrypt')
 
 const { Schema } = mongoose
 
@@ -52,6 +53,21 @@ const UserSchema = new Schema(
     timestamps: true,
   }
 )
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+  try {
+    const salt = await bcrypt.genSalt((saltRounds = 20))
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+  } catch (error) {
+    next(error)
+  }
+})
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password)
+}
 
 UserSchema.plugin(autopopulate)
 
