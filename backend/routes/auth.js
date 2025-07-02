f39ac65ee5c16898e66e4a6041efbfa2f20303d6
davutD <durmaz.dvt@gmail.com>
2025-07-02 23:39:02 +0300

    Add logout endpoint for logging out and me for testing purpose and what is more login and register routes modified

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,10 @@
 const { authService } = require('../services')
+const authHandler = require('../middleware/auth-handler')
 const router = require('express').Router()
 
 router.post('/login', async (req, res, next) => {
   try {
-    const { email, password } = req.body
-    const user = await authService.login(email, password)
+    const user = await authService.login(req)
     res.send(user)
   } catch (err) {
     next(err)
@@ -13,11 +13,25 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/register', async (req, res, next) => {
   try {
-    const newUser = await authService.createUser(req.body)
+    const newUser = await authService.registerUser(req)
     res.status(201).send(newUser)
   } catch (err) {
     next(err)
   }
+})
+
+router.post('/logout', (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return next(err)
+    }
+    res.clearCookie('connect.sid')
+    res.send({ message: 'Logout successful!' })
+  })
+})
+
+router.get('/me', authHandler, (req, res) => {
+  res.send({ user: req.session.user })
 })
 
 module.exports = router
