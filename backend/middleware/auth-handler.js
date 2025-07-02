7fec42a7d3ec46ab1722fff280540094cadf5362
davutD <durmaz.dvt@gmail.com>
2025-07-03 00:25:33 +0300

    Update auth-handler middleware

--- backend/middleware/auth-handler.js
+++ backend/middleware/auth-handler.js
@@ -1,12 +1,14 @@
 const authHandler = (req, res, next) => {
-  if (req.session.user) {
-    return next()
+  if (!req.session.user) {
+    return res.status(401).send({ message: 'You must be logged in.' })
   }
-  res.status(401).send({
-    error: {
-      message: 'You must be logged in to access this resource.',
-    },
-  })
+  if (req.params.userId && req.session.user._id !== req.params.userId) {
+    return res.status(403).send({
+      message: 'Forbidden: You do not have permission to perform this action.',
+    })
+  }
+
+  next()
 }
 
 module.exports = authHandler
