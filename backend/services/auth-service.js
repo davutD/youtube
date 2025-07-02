71a6e7e8017f221c96bf7d0c2655e3d2cdabe6a2
davutD <durmaz.dvt@gmail.com>
2025-07-02 23:40:11 +0300

    Modify login and register auth methods to be able to keep session data

--- backend/services/auth-service.js
+++ backend/services/auth-service.js
@@ -2,24 +2,44 @@ const BaseService = require('./base-service')
 const User = require('../models/user')
 
 class AuthService extends BaseService {
-  async login(email, password) {
+  async login(req) {
+    const { email, password } = req.body
     const user = await this.model.findOne({ email }).select('+password')
     if (!user) {
-      throw new Error('Invalid credentials.')
+      throw Object.assign(new Error('Invalid credentials.'), { status: 401 })
     }
     const isMatch = await user.comparePassword(password)
     if (!isMatch) {
-      throw new Error('Invalid credentials.')
+      throw Object.assign(new Error('Invalid credentials.'), { status: 401 })
     }
-    return this.find(user._id)
+    const userSessionData = {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+      surname: user.surname,
+    }
+    req.session.user = userSessionData
+    return userSessionData
   }
 
-  async createUser(userData) {
+  async registerUser(req) {
+    const userData = req.body
     const user = await this.model.findOne({ email: userData.email })
     if (user) {
-      throw new Error('A user with this email address already exists.')
+      throw Object.assign(
+        new Error('A user with this email address already exists.'),
+        { status: 409 }
+      )
     }
-    return this.model.create(userData)
+    const newUser = await this.model.create(userData)
+    const userSessionData = {
+      _id: newUser._id,
+      email: newUser.email,
+      name: newUser.name,
+      surname: newUser.surname,
+    }
+    req.session.user = userSessionData
+    return userSessionData
   }
 }
 
