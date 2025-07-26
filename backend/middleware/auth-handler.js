52d9d667c0d7d04cb489c60e0c9babd77f3f8773
davutD <durmaz.dvt@gmail.com>
2025-07-27 02:14:55 +0300

    Get user details from session

--- backend/middleware/auth-handler.js
+++ backend/middleware/auth-handler.js
@@ -2,12 +2,7 @@ const authHandler = (req, res, next) => {
   if (!req.session.user) {
     return res.status(401).send({ message: 'You must be logged in.' })
   }
-  if (req.params.userId && req.session.user._id !== req.params.userId) {
-    return res.status(403).send({
-      message: 'Forbidden: You do not have permission to perform this action.',
-    })
-  }
-
+  req.user = req.session.user
   next()
 }
 
