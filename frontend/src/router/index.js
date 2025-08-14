c0002a5c1cee3d441624528a6dcfe5e585f51cf0
davutD <durmaz.dvt@gmail.com>
2025-08-15 00:05:13 +0300

    Update routes for auth routes adn visibility of header component depending on routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SearchView from '../views/SearchView.vue'
 import VideoDetailView from '../views/VideoDetailView.vue'
+import AuthView from '../views/AuthView.vue'
+import LoginForm from '@/components/auth/LoginForm.vue'
+import RegisterForm from '@/components/auth/RegisterForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,18 +13,36 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-      meta: { showSidebar: true },
+      meta: { showSidebar: true, showHeader: true },
     },
     {
       path: '/search',
       name: 'search',
       component: SearchView,
-      meta: { showSidebar: true },
+      meta: { showSidebar: true, showHeader: true },
     },
     {
       path: '/video/:id',
       name: 'video-detail',
       component: VideoDetailView,
+      meta: { showSidebar: false, showHeader: true },
+    },
+    {
+      path: '/auth',
+      component: AuthView,
+      meta: { showHeader: false, showSidebar: false },
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: LoginForm,
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: RegisterForm,
+        },
+      ],
     },
   ],
 })
