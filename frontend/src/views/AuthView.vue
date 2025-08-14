17ff62ad13562c78d1985ff116830bbf23e8dca9
davutD <durmaz.dvt@gmail.com>
2025-08-15 00:05:41 +0300

    Create a basic auth view to render related auth components

---
+++ frontend/src/views/AuthView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
+</script>
+
+<template>
+  <div class="auth-container">
+    <p v-if="authStore.authError" class="error-message">
+      {{ authStore.authError }}
+    </p>
+
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+.auth-container {
+  max-width: 450px;
+  margin: 3rem auto;
+  padding: 2rem;
+  border: 1px solid #373737;
+  border-radius: 8px;
+}
+.error-message {
+  color: red;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+</style>
